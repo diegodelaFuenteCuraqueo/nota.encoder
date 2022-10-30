203196cb61b4f6bd9224b627664a68df3fdd425f
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2022-10-30 17:03:28 -0300

    simple conversions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const ffmpeg = require('fluent-ffmpeg')
+const fs = require('fs')
 const expressFileUpload = require('express-fileupload')
 const app = express()
 
@@ -26,16 +27,43 @@ app.use(
 // index.html convert form
 app.post('/convert', (req,res)=>{
   console.log(" ~ accediendo a /convert")
-  console.log(req)
+  // console.log(req)
   let to = req.body.to
   let file = req.files.file
-  console.log(to)
-  console.log(file)
+  let fileName = `output.${to}`
+  // console.log(to)
+  // console.log(file)
 
   file.mv("tmp/" + file.name, (err) => {
     if (err) return res.sendStatus(500).send(err)
     console.log("archivo subido correctamente")
   })
+
+  // Converts file to especified format
+  ffmpeg("tmp/"+file.name)
+    .withOutputFormat(to)
+    .on('end', (stdout,stderr) => {
+      console.log(" Finalizado")
+      res.download( __dirname + fileName, (err) => {
+        if (err) throw err;
+        fs.unlink( __dirname + fileName, (err) => {
+          if (err) throw err;
+          console.log("File deleted");
+        })
+      })
+      fs.unlink("tmp/"+file.name, (err)=>{
+        if (err) throw err
+        console.log("archivo borrado")
+      })
+    })
+    .on('error', (err)=>{
+      console.log("error:",err)
+      fs.unlink( __dirname + fileName, (err) => {
+        if (err) throw err;
+        console.log("File deleted");
+      })
+    })
+    .saveToFile(__dirname + fileName)
 
 })
 
